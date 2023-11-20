489c40b11ae5c9e736fcb08f2807e66d9b29c41e
enomskey <enomskey@yahoo.com>
2023-11-20 16:06:34 +0100

    create new file

---
+++ src/app/page.jsx
@@ -0,0 +1,12 @@
+export default function page() {
+  return (
+    <main>
+      <h1>hello react</h1>
+
+<section>
+    <h2>hello next</h2>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi sint dolor, aut facilis numquam laboriosam unde? Consequatur, voluptatem, doloribus, fugit officia atque ad magni autem vero sed non aliquid quis?</p>
+</section>
+    </main>
+  )
+}
